1cd0c9c047e1ee52adbd7f3223e456e7c824726a
Hajdu Tomi <hajdu.tomi@szatyi.hu>
2025-09-22 12:43:34 +0200

    Urlap_folyt

--- src/component/Urlap.jsx
+++ src/component/Urlap.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
 function Urlap(){
     const [szoveg, setSzoveg] = useState('');
-    const [user, setUser] = useState({nev: ''});
+    const [user, setUser] = useState({nev: '', evf: 13});
+    const kuldes = (event) => {
+        event.preventDefault();
+    }
     return(
         <>
             <input type="text" 
@@ -9,7 +12,7 @@ function Urlap(){
             onChange={event => 
             Setszoveg(event.target.value)}/>
             <p>{szoveg}</p>
-            <form>
+            <form onSubmit={kuldes}>
                 <fieldset>
                     <legend>Adatok</legend>
                     <label htmlFor="nev">Név</label>
@@ -18,9 +21,18 @@ function Urlap(){
                     value={user.nev} 
                     onChange={e => setUser({...user, nev: e.target.value})}
                     id="nev" />
+
+                    <br />
+                    <label htmlFor="evf">Évfolyam</label>
+                    <select name="evf" id="evf" value={user.evf} onChange={e => setUser({evf: e.target.value, ...user})}>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                    </select>
                 </fieldset>
             </form>
-            <p>Helló {user.nev}</p>
+            <p>Helló {user.nev}({user.evf}) évfolyam</p>
         </>
     )
 }
