c34899348027e7e76c464e1a4ebee6fec4ef62af
Hajdu Tomi <hajdu.tomi@szatyi.hu>
2025-09-22 12:32:18 +0200

    Mukodo_urlap

--- src/component/Urlap.jsx
+++ src/component/Urlap.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react"
 function Urlap(){
-    const [szoveg, Setszoveg] = useState('');
+    const [szoveg, setSzoveg] = useState('');
+    const [user, setUser] = useState({nev: ''});
     return(
         <>
-            <input type="text" value={szoveg} onChange={event => Setszoveg(event.target.value)}/>
+            <input type="text" 
+            value={szoveg} 
+            onChange={event => 
+            Setszoveg(event.target.value)}/>
             <p>{szoveg}</p>
+            <form>
+                <fieldset>
+                    <legend>Adatok</legend>
+                    <label htmlFor="nev">Név</label>
+                    <input type="text" 
+                    name="nev"
+                    value={user.nev} 
+                    onChange={e => setUser({...user, nev: e.target.value})}
+                    id="nev" />
+                </fieldset>
+            </form>
+            <p>Helló {user.nev}</p>
         </>
     )
 }
