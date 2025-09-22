bf1712d1aa60b1c818993f1ded0dabf588ee8be0
Hajdu Tomi <hajdu.tomi@szatyi.hu>
2025-09-22 11:01:53 +0200

    ujj

--- src/component/Flip.jsx
+++ src/component/Flip.jsx
@@ -1,9 +1,15 @@
+import styles from '../css/flip.css'
 function Flip(){
     var hit = Math.random() < 0.5
-    
+
     return(
         <>
         </>
     )
+    function piros()
+    {
+        return <div className={styles.Flip}></div>
+    }
+
 }
 export default Flip
